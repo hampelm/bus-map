80ae753d74cbf54bc754576c6cf6bca47b76bac7
Matt Hampel <matth@localdata.com>
2016-04-25 18:04:24 -0400

    Add route lines to bus map

--- js/app.js
+++ js/app.js
@@ -11,7 +11,10 @@ $(function(){
   var busDots = L.layerGroup().addTo(map);
 
   var apiURL = 'http://ddot-beta.herokuapp.com/api/api/where/';
-  var key = 'LIVEMAP';
+  var apiKey = 'LIVEMAP';
+
+  var layers = {};
+  var geojsons = {};
 
   var style = {
     stroke: false,
@@ -19,6 +22,22 @@ $(function(){
     fillOpacity: 1,
     radius: 5
   };
+
+  var ROUTE_STYLE = {
+    color: '#0062be',
+    opacity: 1,
+    weight: 3
+  };
+  var CIRCLE_ROUTE_STYLE = {
+    color: '#ffad00',
+    opacity: 1,
+    fillColor: '#ffad00'
+  };
+  var RADIUS = 12;
+
+  var layers = {};
+  var geojsons = {};
+  var circles = [];
 
   var getStyle = function(d) {
     var s = _.clone(style);
@@ -30,6 +49,76 @@ $(function(){
     if (d >= 20) { s.fillColor ="#e20027"; }
     return s;
   }
+
+  var getRouteStyle = function(d) {
+    var s = _.clone(ROUTE_STYLE);
+
+    s.color = '#' + d.color;
+
+    return s;
+  };
+
+  var dim = function() {
+    _.each(layers, function(layer) {
+      var options = layer.options;
+      options.opacity = 0.1;
+      options.dashArray = '';
+      layer.setStyle(options);
+    });
+  };
+
+  var light = function(key) {
+    var options = layers[key].options;
+    options.opacity = 0.6;
+    options.dashArray = '4,4';
+    layers[key].setStyle(options);
+  };
+
+  var bright = function(key) {
+    var options = layers[key].options;
+    options.opacity = 1;
+    options.dashArray = '';
+    layers[key].setStyle(options);
+  };
+
+  var showOverlaps = function(keys) {
+    _.each(layers, dim);
+    _.each(keys, light);
+  };
+
+  var addCircle = function(geoJSON) {
+    var ll = new L.LatLng(geoJSON.coordinates[0], geoJSON.coordinates[1]);
+    var circle = L.circle(ll, RADIUS, CIRCLE_ROUTE_STYLE);
+    map.addLayer(circle);
+    circles.push(circle);
+  };
+
+  var removeCircles = function() {
+    _.each(circles, function(circle) {
+      map.removeLayer(circle);
+    });
+    circles = [];
+  };
+
+  var findOverlaps = function(event) {
+    var source = event.target.toGeoJSON();
+    var routeId = event.target._routeId;
+    var keys = [];
+
+    removeCircles();
+
+    _.each(geojsons, function(value, key) {
+      if(key === routeId) { return; } // don't self-intersect
+      var intersects = gju.lineStringsIntersect(source.geometry, value.geometry);
+      if (intersects) {
+        _.each(intersects, addCircle);
+        keys.push(key);
+      }
+    });
+
+    showOverlaps(keys);
+    bright(routeId);
+  };
 
   var success = function(data) {
     // console.log(data);
@@ -55,18 +144,78 @@ $(function(){
 
         var marker = L.circleMarker(ll, s);
 
-        marker.on('mouseover', function() {
+        marker.on('mouseover', function(event) {
           $('#route').html(trips[bus.tripId].tripHeadsign.toUpperCase());
+          event.target._routeId = trips[bus.tripId].routeId;
+          handleClick(event);
         });
         busDots.addLayer(marker);
       }
     });
   }
 
-  var debounced;
+  var handleClick = function(event, route) {
+    var routeId = event.target._routeId;
+    findOverlaps(event);
+  };
 
+  var gotRoute = function(data) {
+    var route = data.data.entry;
+    var references = data.data.references;
+    var details = _.where(references.routes, { id: route.routeId })[0]; // headsign etc
+    var style = getRouteStyle(details);
+
+    // console.log(route);
+    // console.log(references);
+    // console.log(details);
+
+    var sortedPolylines = _.sortBy(route.polylines, function(l) { return -l.length; });
+    var encodedPolyline = sortedPolylines[0].points;
+    var polyline = L.Polyline.fromEncoded(encodedPolyline).setStyle(style).addTo(map);
+    polyline._routeId = route.routeId;
+
+    layers[route.routeId] = polyline;
+    geojsons[route.routeId] = polyline.toGeoJSON();
+
+    polyline.on('click', handleClick);
+  };
+
+  var fetchRoute = function(route) {
+    var jqxhr = $.ajax(apiURL + 'stops-for-route/' + route.id + '.json?key=' + apiKey, {
+      dataType: 'json'
+    });
+
+    jqxhr.done(gotRoute);
+
+    jqxhr.fail(function(error) {
+      console.log(error);
+    });
+  };
+
+  var gotRoutes = function(data) {
+    var routes = data.data.list;
+
+    _.each(routes, fetchRoute);
+  };
+
+  var fetchRoutes = function() {
+    var jqxhr = $.ajax(apiURL + 'routes-for-agency/DDOT.json?key=' + apiKey, {
+      dataType: 'json'
+    });
+
+    jqxhr.done(gotRoutes);
+
+    jqxhr.fail(function(error) {
+      console.log(error);
+    });
+  };
+
+  fetchRoutes();
+
+
+  var debounced;
   var fetch = function() {
-    var jqxhr = $.ajax(apiURL + 'vehicles-for-agency/DDOT.json?key=' + key, {
+    var jqxhr = $.ajax(apiURL + 'vehicles-for-agency/DDOT.json?key=' + apiKey, {
       dataType: 'json'
     });
 
